d50e33c0f18cfd47152ee0ae943db28f71de44e3
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-29 21:40:59 -0700

    Sample tests pass, sinatra app loads and runs. Test pages display.

--- scaffolds/sinatra_ruby/spec/integration_spec.rb
+++ scaffolds/sinatra_ruby/spec/integration_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+describe 'test path', type: :feature do
+  it 'will navigate to the homepage, then to the test page' do
+    visit '/'
+    click_on 'Test'
+    expect(page).to have_content 'Everything\'s good.'
+  end
+end
